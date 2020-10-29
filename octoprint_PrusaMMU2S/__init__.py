282b787f0e70a53df4fa8de3ee37221bf815f466
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2020-10-29 16:51:05 -0400

    Update __init__.py

--- octoprint_PrusaMMU2S/__init__.py
+++ octoprint_PrusaMMU2S/__init__.py
@@ -2,7 +2,9 @@
 from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
-
+from past import basestring
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplatePlugin):
     def on_after_startup(self):
         self._logger.info("Hello World!")
+        hooks = self._plugin_manager.get_hooks("octoprint.comm.transport.serial.factory")
+        
