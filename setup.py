c8bea347b977fa53b1ef6eccb2a350c7adeab08e
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2020-10-29 16:15:04 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+__plugin_name__ = "Prusa MMU2S"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "Recieves messages from MMU2S"
+__plugin_pythoncompat__ = ">=2.7,<4"
