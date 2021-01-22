eb2c2599c62c1789e6302450b0d0a42dfed2a41b
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2021-01-21 20:24:54 -0500

    Update __init__.py

--- octoprint_PrusaMMU2S/__init__.py
+++ octoprint_PrusaMMU2S/__init__.py
@@ -5,6 +5,41 @@ import octoprint.plugin
 from past import basestring
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplatePlugin):
     def on_after_startup(self):
-        self._logger.info("Hello World!")
+        self.potentialError=False
+        self.Error=False
+        self._logger.info("Plugin Started!")
         hooks = self._plugin_manager.get_hooks("octoprint.comm.transport.serial.factory")
+        if error:
+            self._plugin_manager.send_plugin_message(self._identifier, dict(type="error", msg="MMU needs user attention"))
+    def custom_action_handler(self, comm, line, action, *args, **kwargs):
         
+        if line == "mmu_get_response - begin move: load":
+            self._plugin_manager.send_plugin_message(self._identifier, dict(type="complete", msg="MMU Is loading"))
+        else if line =="Recv: mmu_get_response() returning: 0":
+            self.potentialError=True
+            self._plugin_manager.send_plugin_message(self._identifier, dict(type="error", msg="Something may be wrong with the MMU"))
+        else if line=="Recv: MMU not responding":
+            self.potentialError=True
+            if !self.potentialError:
+                self._plugin_manager.send_plugin_message(self._identifier, dict(type="error", msg="Something may be wrong with the MMU"))
+            
+        else if potentialError and line =="Recv: echo:busy: paused for user":
+            error= True
+            self._plugin_manager.send_plugin_message(self._identifier, dict(type="error", msg="MMU needs user attention"))
+        else:
+            return
+        
+
+        self._logger.info("Received \"custom\" action from printer")
+
+__plugin_name__ = "MMU2S Announcement Plugin"
+__plugin_pythoncompat__ = ">=2.7,<4"
+
+def __plugin_load__():
+    plugin = CustomActionCommandPlugin()
+
+    global __plugin_implementation__
+    __plugin_implementation__ = plugin
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {"octoprint.comm.protocol.action": plugin.custom_action_handler}
